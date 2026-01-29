cdf014b2a4c1d8581a1099e9285d9a676fbd349e
imi <imi@1m1.io>
2026-01-29 10:02:52 -0800

    .

--- wip/∃.jl
+++ wip/∃.jl
@@ -6,14 +6,17 @@ I = [ZERO < ○ < ONE] denotes a unit 1-dim space of information with origin ○
 * ∃!ϵ̂: ϵ ∈ ϵ̂ ∈ ∃: ϵ|ϵ̂ ⊆ ϵ̂
 * ∀ ϵ̂ ≠ ϵ ∈ ∃: ϵ̂ ∩ ϵ = ∅
 * ϵ.∃ ∈ I is arbitrary, computable and smooth fuzzy existence towards ONE=true xor ZERO=false.
+
+ϵ ∈ ∃ defines its existence inside an Ω using an origin vector (μ) and a radius vector (ρ), these vectors are finite and all other dimensional coordinates of ϵ follow from stepwise interpolation defined in the ∂ vector.
+If we use a horizontal axis for dimension and a vertical axis for coordinate in the dimension, for any ϵ, the chart looks like a stepwise constant function with finite jumps.
 """
-# module TheoryOfGod
+module TheoryOfGod
 
 using Serialization
 
 ○(::Type{T}) where {T<:Real} = one(T) / (one(T) + one(T))
 abstract type Topology{T<:Real} end
-struct Ω{T<:Real} <: Topology{T}
+struct ∀{T<:Real} <: Topology{T}
     ϵ::Vector{Topology{T}}
 end
 struct ∃{T<:Real} <: Topology{T}
@@ -26,24 +29,10 @@ struct ∃{T<:Real} <: Topology{T}
     ∃̂::Topology{T}
     ϵ::Vector{∃{T}}
 end
-∩(::∃, ::Ω) = true
-⩀(::∃, ::Ω) = true
-∃(::∃{T}, ::Any, ::Ω) where {T<:Real} = ○(T)
-isroot(::Ω) = true
-isroot(::∃) = false
-origin(::Ω{T}) where {T<:Real} = ○(T)
-origin(ϵ::∃) = ϵ.μ
-radius(::Ω{T}) where {T<:Real} = ○(T)
-radius(ϵ::∃) = ϵ.ρ
-zero(ϵ::Topology) = origin(ϵ) - radius(ϵ)
-one(ϵ::Topology) = origin(ϵ) + radius(ϵ)
-# dimension()
-
 index(n::Vector{<:Integer}) = (collect(i) for i ∈ Iterators.product((1:n̂ for n̂ ∈ n)...))
-function ∃(n::Vector{<:Integer}, ϵ::∃{T}, Ξ::Dict{∃{T},T}=Dict()) where {T<:Real}
-    isroot(ϵ) && return throw("Cannot observe all of Ω")
+function ∃(n::Vector{<:Integer}, ϵ::∃{T}, Ξ::Dict{<:Topology{T},T}=Dict()) where {T<:Real}
     ∃̂ = fill(○(T), n...)
-    _zero = zero(ϵ)
+    _zero = ϵ.μ - ϵ.ρ
     for i = index(n)
         μ = fill(○(T), length(n))
         for î = eachindex(ϵ.d)
@@ -58,7 +47,7 @@ function ∃(n::Vector{<:Integer}, ϵ::∃{T}, Ξ::Dict{∃{T},T}=Dict()) where 
     end
     ∃̂
 end
-function ∂(x::∃{T}, ϵ::∃{T}) where {T<:Real}
+function ∂(x::∃, ϵ::∃)
     for (i, d) = enumerate(ϵ.d)
         iszero(ϵ.ρ[i]) && continue
         x̂ = μ(x, d)
@@ -68,7 +57,6 @@ function ∂(x::∃{T}, ϵ::∃{T}) where {T<:Real}
     false
 end
 function μ(ϵ::∃{T}, d::T)::T where {T<:Real}
-    isroot(ϵ) && return ○(T) # needed?
     isempty(ϵ.d) && return ○(T)
     for (i, d̂) = enumerate(ϵ.d)
         d == d̂ && return ϵ.μ[i]
@@ -78,12 +66,12 @@ function μ(ϵ::∃{T}, d::T)::T where {T<:Real}
     _one = isone(d̂[end]) ? ϵ.d[end] : ○(T)
     if d < d̂[1]
         ∂ = ϵ.∂[1]
-        ∂ == :ZEROONE && return _zero
-        ∂ == :ONEZERO && return ○(T)
+        ∂ == :ZEROONE && return ϵ.μ[1]
+        ∂ == :ONEZERO && return _zero
     elseif d̂[end] < d
         ∂ = ϵ.∂[end]
-        ∂ == :ZEROONE && return ○(T)
-        ∂ == :ONEZERO && return _one
+        ∂ == :ZEROONE && return _one
+        ∂ == :ONEZERO && return ϵ.μ[end]
     else
         i = findlast(d̃ -> d̃ < d, d̂)
         ∂ = ϵ.∂[i+1]
@@ -101,25 +89,17 @@ function ∃(x::∃{T}, ϵchecked::Union{∃{T},Nothing}, ϵ̂::∃{T}) where {T
         x ⩀ ϵ̃ || continue
         x ∩ ϵ̃ && return ϵ̃.∃(x)
     end
-    isnothing(ϵ.∃̂) && return ○(T)
+    isnothing(ϵ̂.∃̂) && return ○(T)
     ∃(x, ϵ̂, ϵ̂.∃̂)
 end
 closed_zero(mode::Symbol) = mode == :ONEZERO || mode == :ONEONE
 closed_one(mode::Symbol) = mode == :ZEROONE || mode == :ONEONE
 function ∅(zero₁, one₁, czero₁, cone₁, zero₂, one₂, czero₂, cone₂)
-    if cone₁ && czero₂
-        one₁ < zero₂ && return true
-    else
-        one₁ ≤ zero₂ && return true
-    end
-    if cone₂ && czero₁
-        one₂ < zero₁ && return true
-    else
-        one₂ ≤ zero₁ && return true
-    end
+    (cone₁ && czero₂ ? one₁ < zero₂ : one₁ ≤ zero₂) && return true
+    (cone₂ && czero₁ ? one₂ < zero₁ : one₂ ≤ zero₁) && return true
     false
 end
-function ⩀(ϵ::∃{T}, ϵ̂::∃{T}) where {T<:Real}
+function ⩀(ϵ::∃, ϵ̂::∃)
     for (i, d) ∈ enumerate(ϵ̂.d)
         d ∈ ϵ.d && continue
         x = μ(ϵ, d)
@@ -128,71 +108,59 @@ function ⩀(ϵ::∃{T}, ϵ̂::∃{T}) where {T<:Real}
     end
     true
 end
-# function ∩(ϵ::∃{T}, ϵ̂::∃{T}) where {T<:Real}
-#     !(ϵ ⩀ ϵ̂) && return false
-#     !(ϵ̂ ⩀ ϵ) && return false
-#     for (i, d) ∈ enumerate(ϵ.d)
-#         j = findfirst(==(d), ϵ̂.d)
-#         isnothing(j) && continue
-#         zero₁, one₁ = ϵ.μ[i] - ϵ.ρ[i], ϵ.μ[i] + ϵ.ρ[i]
-#         zero₂, one₂ = ϵ̂.μ[j] - ϵ̂.ρ[j], ϵ̂.μ[j] + ϵ̂.ρ[j]
-#         ∅(
-#             zero₁,
-#             one₁, 
-#             closed_zero(ϵ.∂[i]), 
-#             closed_one(ϵ.∂[i+1]), 
-#             zero₂, 
-#             one₂, 
-#             closed_zero(ϵ̂.∂[j]), 
-#             closed_one(ϵ̂.∂[j+1])) && return false
-#     end
-#     n, m = length(ϵ.d), length(ϵ̂.d)
-#     if 0 < n && 0 < m
-#         mode₁ = ϵ.∂[n+1]
-#         mode₂ = ϵ̂.∂[m+1]
-#         mode₁ ≠ mode₂ && return false
-#         if mode₁ == :ZEROONE
-#             tail₁ = ϵ.μ[n]
-#             tail₂ = ϵ̂.μ[m]
-#             tail₁ ≠ tail₂ && return false
-#         end
-#     end
-#     isempty(ϵ̂.ϵ) && return true
-#     any(ϵ̃ -> ϵ ∩ ϵ̃, ϵ̂.ϵ)
-# end
-# function ∃!(ι::String, d::Vector{T}, μ::Vector{T}, ρ::Vector{T},
-#             ∂::Vector{Symbol}, E::Function) where {T<:Real}
-#     ϵ = ∃{T}(ι, d, μ, ρ, ∂, E, ϵ, [])
-#     for ϵ̂ = ϵ.ϵ
-#         ϵ ⩀ ϵ̂ || continue
-
-#         ϵ ∩ ϵ̂ && return nothing
-#         ϵ ∩ ϵ̃
-#     end
-#     push!(ϵ.ϵ, ϵ̂)
-#     ϵ̂
-# end
-function ∃!(ϵ::∃)
+function Base.:∩(ϵ::∃, ϵ̂::∃)
+    ϵ ⩀ ϵ̂ || return false
+    ϵ̂ ⩀ ϵ || return false
+    for (i, d) ∈ enumerate(ϵ.d)
+        j = findfirst(==(d), ϵ̂.d)
+        isnothing(j) && continue
+        zero₁, one₁ = ϵ.μ[i] - ϵ.ρ[i], ϵ.μ[i] + ϵ.ρ[i]
+        zero₂, one₂ = ϵ̂.μ[j] - ϵ̂.ρ[j], ϵ̂.μ[j] + ϵ̂.ρ[j]
+        ∅(
+            zero₁,
+            one₁, 
+            closed_zero(ϵ.∂[i]), 
+            closed_one(ϵ.∂[i+1]), 
+            zero₂, 
+            one₂, 
+            closed_zero(ϵ̂.∂[j]), 
+            closed_one(ϵ̂.∂[j+1])) && return false
+    end
+    n, m = length(ϵ.d), length(ϵ̂.d)
+    if 0 < n && 0 < m
+        mode₁ = ϵ.∂[n+1]
+        mode₂ = ϵ̂.∂[m+1]
+        mode₁ ≠ mode₂ && return false
+        if mode₁ == :ZEROONE
+            tail₁ = ϵ.μ[n]
+            tail₂ = ϵ̂.μ[m]
+            tail₁ ≠ tail₂ && return false
+        end
+    end
+    isempty(ϵ̂.ϵ) && return true
+    any(ϵ̃ -> ϵ ∩ ϵ̃, ϵ̂.ϵ)
+end
+function ∃!(ϵ::∃, Ω::∀)
     intersects, ϵ̂ = ϵ ∩ Ω
     intersects && return nothing
-    ϵ̃ = ∃{eltype(ϵ.d)}(ϵ.ι, ϵ.d, ϵ.μ, ϵ.ρ, ϵ.∂, ϵ.E, ϵ̂, ϵ.ϵ)
+    ϵ̃ = ∃{eltype(ϵ.d)}(ϵ.ι, ϵ.d, ϵ.μ, ϵ.ρ, ϵ.∂, ϵ.∃, ϵ̂, ϵ.ϵ)
     push!(ϵ̂.ϵ, ϵ̃)
     ϵ̃
 end
-function ∩(ϵ::Topology{T}, ϵ′::Topology{T}, ϵ̃::Topology{T}, ϵ̃depth<:Integer, ϵ̂depth<:Integer)
-    ϵ ⩀ ϵ̃ || return 
-    if ϵ̂depth < ϵ̃depth
-        ϵ′ = ϵ̃
-        ϵ̂depth = ϵ̃depth
-    end
-    ϵ ∩ ϵ̃ && return true
-    for ϵ́ in ϵ̃.ϵ
-        ∩(ϵ, ϵ′, ϵ́, ϵ̃depth + 1, ϵ̂depth)
-    end
+function Base.:∩(ϵ::∃, ϵ̂::Topology)
+    ϵ′ = Ref{Topology}(ϵ̂)
+    ϵdepth = Ref(0)
+    intersects = any(ϵ̃ -> ∩(ϵ, ϵ̃, 1, ϵ′, ϵdepth), ϵ̂.ϵ)
+    intersects, ϵ′[]
 end
-function ∩(ϵ::∃{T}, ϵ̂::Topology{T}) where {T<:Real}
-    any(ϵ̃ -> ∩(ϵ, ϵ̂, ϵ̃, 1, 0), ϵ̂.ϵ) && return true, ϵ̂
-    false, ϵ̂
+function Base.:∩(ϵ::∃, ϵ̂::Topology, ϵ̂depth::Int, ϵ′::Ref{Topology}, ϵdepth::Ref{<:Integer})
+    ϵ ⩀ ϵ̂ || return false
+    if ϵdepth[] < ϵ̂depth
+        ϵ′[] = ϵ̂
+        ϵdepth[] = ϵ̂depth
+    end
+    ϵ ∩ ϵ̂ && return true
+    any(ϵ̃ -> ∩(ϵ, ϵ̃, ϵ̂depth + 1, ϵ′, ϵdepth), ϵ̂.ϵ)
 end
 
-# end
+end
