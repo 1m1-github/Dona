8e61fd06a5117df046b597088343e73550e947e8
1m1 <imi@1m1.io>
2026-04-22 13:58:18 -0600

    .

---
+++ src/Dona.jl
@@ -0,0 +1,27 @@
+module Dona
+
+const ROOT = @__DIR__
+
+include("Logging.jl")
+
+# julia --quiet --depwarn=error --threads auto main.jl Dona
+
+# const GROUP = ARGS[1]
+# const LONG_MEMORY = joinpath(ROOT, "$NAME/long")
+# const BOOT = joinpath(ROOT, "$NAME/boot")
+# cd(LONG_MEMORY)
+
+# include(joinpath(ROOT, "knowledge/LoopOSPkg.jl"))
+# LoopOSPkg.@install "https://github.com/1m1-github/LoopOS.git"
+# include("../LoopOS/src/LoopOS.jl")
+
+# const MIN_BOOT = ["Pkg", "State", "Caching", "Learning", "AnthropicIntelligence", "Intelligence", "ZMQ"]
+
+# load(BOOT)
+# LoopOS.awaken(BOOT)
+
+function (@main)(ARGS)
+    print("hi", ARGS)
+end
+
+end
